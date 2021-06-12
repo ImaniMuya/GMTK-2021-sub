7eee100bcdff4ab89b3166944cd811375e41d792
ImaniMuya <imani.muya@gmail.com>
2021-06-12 00:44:40 -0500

    smooth collision

--- collisionMap.js
+++ collisionMap.js
@@ -1,27 +1,76 @@
 // Edric Yu
 
-function attemptInit() {
-  if (loadedImgs == 1) {
-    ctx.drawImage(c_map, 0, 0);
-    map_data = getCanvasData();
-    loadmsg.parentNode.removeChild(loadmsg);
-    gameInt = setInterval(tick, 1);
-    flushToCanvas();
-    clearInterval(loadInterval);
-  }
-}
-
-function getCanvasData() {
+function getCanvasData(ctx) {
   data = [];
-  raw_data = ctx.getImageData(0, 0, W, H).data;
-  for (let row = 0; row < H; row++) {
+  raw_data = ctx.getImageData(0, 0, SIZE, MAP_H).data;
+  for (let row = 0; row < MAP_H; row++) {
     data.push([]);
-    for (let col = 0; col < W; col++) {
-      let pos = 4 * (col + row * W);
+    for (let col = 0; col < SIZE; col++) {
+      let pos = 4 * (col + row * SIZE);
       let colorstr = raw_data.slice(pos, pos + 4).toString();
       let tileid = TILES[colorstr];
       data[row].push(tileid);
     }
   }
   return data;
+}
+
+// returns x pos of collision or null
+function getCollisionDown(bottom, x, width) {
+  let r = Math.round(bottom) + 1; //row below bottom
+  if (r < 0) return null;
+  if (r >= MAP_H) return x; //screen bottom
+  x = Math.round(x); //left
+  for (let c = x; c < x + width; c++) {
+    if (map_data[r][c] !== null) {
+      return c;
+    }
+  }
+  return null;
+}
+
+// returns x pos of collision or null
+function getCollisionUp(top, x, width) {
+  let r = Math.round(top) - 1; //row above top
+  if (r < 0) return x; //screen top
+  x = Math.round(x); //left
+  for (let c = x; c < x + width; c++) {
+    if (map_data[r][c] == 1) {
+      return c;
+    }
+  }
+  return null;
+}
+
+// returns y pos of collision or null
+function getCollisionRight(right, bottom, height) {
+  let c = Math.round(right) + 1; //col after right
+  if (c > SIZE) return bottom; //screen right
+  bottom = Math.round(bottom); //bottom
+  for (let r = bottom - height + 1; r <= bottom; r++) {
+    //scan downward to find highest collision
+    if (map_data[r][c] == 1) {
+      return r;
+    }
+  }
+  return null;
+}
+
+// returns y pos of collision or null
+function getCollisionLeft(left, bottom, height) {
+  let c = Math.round(left) - 1; //col before left
+  if (c < 0) return bottom; //screen left
+  bottom = Math.round(bottom); //bottom
+  for (let r = bottom - height + 1; r <= bottom; r++) {
+    //scan downward
+    if (map_data[r][c] == 1) {
+      return r;
+    }
+  }
+  return null;
+}
+
+function flushToCanvas() {
+  vis_ctx.drawImage(renderer, 0, 0); //flush to canvas
+  requestAnimationFrame(flushToCanvas);
 }
--- submarine.js
+++ submarine.js
@@ -1,6 +1,6 @@
 class Submarine {
-  x = 0;
-  y = 0;
+  x = 500;
+  y = 500;
 
   vx = 0;
   vy = 0;
@@ -21,20 +21,72 @@ class Submarine {
     if (diver.isOutOfSub) {
       return;
     }
+    if (keys["a"]) {
+      this.vx = -this.speed;
+    }
+    if (keys["d"]) {
+      this.vx = +this.speed;
+    }
 
-    if (keys["a"]) this.vx = -this.speed;
-    if (keys["d"]) this.vx = +this.speed;
+    let vx = Math.round(this.vx);
+    let bottom = this.y + this.h;
+    let right = this.x + this.w;
+
+    if (vx > 0) {
+      //moving right
+      for (let i = 0; i < vx; i++) {
+        let y_cls = getCollisionRight(right + 1, bottom, this.h);
+        if (y_cls == null) {
+          this.x++;
+        } else {
+          //hit wall
+          this.vx = 0;
+        }
+      }
+    } else if (vx < 0) {
+      //moving left
+      for (let i = 0; i > vx; i--) {
+        let y_cls = getCollisionLeft(this.x, bottom, this.h);
+        if (y_cls == null) {
+          this.x--;
+        } else {
+          //hit wall
+          this.vx = 0;
+        }
+      }
+    }
 
     if (keys[" "]) {
       particles.explode(this.x, this.y, "pink");
     }
 
-    this.x += this.vx;
-    this.y += 1.25;
+    this.vy = 2;
+    if (this.vy > 0) {
+      //moving down
+      for (let i = 0; i < this.vy; i++) {
+        let x_cls = getCollisionDown(bottom, this.x, this.w);
+        if (x_cls == null) {
+          this.y++;
+        } else {
+          //landed on something
+          this.vy = 0;
+        }
+      }
+    } else if (this.vy < 0) {
+      //moving up
+      for (let i = 0; i > this.vy; i--) {
+        // let x_cls = getCollisionUp(this.y, this.x, this.w);
+        // if (x_cls == null) {
+        //   this.y--;
+        // } else {
+        //   this.vy = 0;
+        // }
+      }
+    }
   }
-  reset(xcord,ycord) {
-    this.x = xcord
-    this.y = ycord
+  reset(xcord, ycord) {
+    this.x = xcord;
+    this.y = ycord;
   }
 
   draw(ctx) {
