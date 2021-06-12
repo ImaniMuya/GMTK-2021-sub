8630e02973827e8dcc2b6653828e021e2e236a4a
ImaniMuya <imani.muya@gmail.com>
2021-06-11 19:25:19 -0500

    diver in and out of sub

--- diver.js
+++ diver.js
@@ -5,29 +5,42 @@ class Diver {
   vx = 0;
   vy = 0;
 
-  w = 200;
-  h = 200;
-  speed = 5;
+  w = 20;
+  h = 30;
+  speed = 1;
+  isOutOfSub = false;
+
+  get cx() {
+    return this.x + this.w / 2;
+  }
+  get cy() {
+    return this.y + this.h / 2;
+  }
 
   update(keys) {
-    this.vx = 0;
-    this.vy = 0;
+    if (this.isOutOfSub) {
+      this.vx = 0;
+      this.vy = 0;
 
-    if (keys["ArrowLeft"]) this.vx = -this.speed;
-    if (keys["ArrowRight"]) this.vx = +this.speed;
-    if (keys["ArrowUp"]) this.vy = -this.speed;
-    if (keys["ArrowDown"]) this.vy = +this.speed;
+      if (keys["ArrowLeft"]) this.vx = -this.speed;
+      if (keys["ArrowRight"]) this.vx = +this.speed;
+      if (keys["ArrowUp"]) this.vy = -this.speed;
+      if (keys["ArrowDown"]) this.vy = +this.speed;
 
-    if (keys[" "]) {
-      particles.explode(this.x, this.y, "blue");
+      if (keys[" "]) {
+        particles.explode(this.x, this.y, "blue");
+      }
+
+      this.x += this.vx;
+      this.y += this.vy;
+    } else {
+      this.x = submarine.x;
+      this.y = submarine.y;
     }
-
-    this.x += this.vx;
-    this.y += this.vx;
   }
 
   draw(ctx) {
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "blue";
     ctx.fillRect(this.x, this.y, 20, 30);
   }
 }
